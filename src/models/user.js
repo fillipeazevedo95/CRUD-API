52d5be786ccc14cf70b93abf595be0c86e54e83d
Fillipe Azevedo <fillipedev95@gmail.com>
2021-11-24 14:46:18 -0300

    configuração do models/user.js criação do modelo de usuario

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+
+const userShema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userShema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWPRIVATEKEY);
+    return token;
+};
+
+const User = mongoose.model('user', userShema);
+
+const validate = (user) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        emil: Joi.string().email().required(),
+        password: Joi.string().required()
+    });
+    return schema.validate(user);
+};
+
+module.exports = { User, validate };
